0e1596727149dbd71ac4b1fb3be61e66c8340430
Frank de Jonge <info@frankdejonge.nl>
2025-05-11 13:41:27 +0200

    Build before testing, always.

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,6 +1,24 @@
 import { defineConfig } from 'vitest/config';
+import tsconfigPaths from 'vite-tsconfig-paths';
+import path from 'node:path';
+
+console.log('here', new URL(import.meta.url).pathname);
 
 export default defineConfig({
+    plugins: [tsconfigPaths()],
+    resolve: {
+        alias: [
+            {
+                // new URL('./src/', import.meta.url).pathname,
+                find: /^@flystorage\/(.*)$/,
+                replacement: path.join(new URL(import.meta.url).pathname, './packages', '$1'),
+            }
+        ]
+        // alias: {
+        //     '@flystorage/file-storage': './packages/file-storage/src/index.ts',
+        //     // Add other packages as needed
+        // }
+    },
     test: {
         globals: true,
         clearMocks: false,
