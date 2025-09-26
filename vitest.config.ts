2dc8dcab18d2c609a2c3cacbece5e9c24e5d5b72
Frank de Jonge <info@frankdejonge.nl>
2025-09-26 17:19:27 +0200

    Do not use all lock files for npm cache hash

--- vitest.config.ts
+++ vitest.config.ts
@@ -2,7 +2,6 @@ import { defineConfig } from 'vitest/config';
 import tsconfigPaths from 'vite-tsconfig-paths';
 import path from 'node:path';
 
-
 export default defineConfig({
     plugins: [tsconfigPaths()],
     resolve: {
